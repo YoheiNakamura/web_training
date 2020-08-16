f5cc4c449476ce650f023e5278c87552670d402f
YoheiNakamura <60618933+YoheiNakamura@users.noreply.github.com>
2020-08-17 02:51:18 +0900

    sidebar etc

--- 9999_練習用/scripts/modal.js
+++ 9999_練習用/scripts/modal.js
@@ -1,22 +1,96 @@
-//modalで使うやつです
-Vue.component('modal', {
-    template: '#modal-template'
+
+Vue.component('item-modal', {
+    template: '#item-modal-template',
+    methods: {
+        clickEvent: function () {
+            this.$emit('from-child')
+        }
+    }
 })
+Vue.component('thread-modal', {
+    template: '#thread-modal-template',
+    methods: {
+        clickEvent: function () {
+            this.$emit('from-child')
+        }
+    }
+})
+
 
 Vue.component('modal-image', {
     template: '#modal-image-template'
 })
- 
+
+Vue.component('open-modal', {
+    template: '#sample-template',
+    methods: {
+        clickEvent: function () {
+            this.$emit('from-child')
+        }
+    }
+})
+
+new Vue({
+    el: '#sample',
+    data: {
+        show: false
+    },
+    methods: {
+        openModal: function () {
+            this.show = true;
+            
+        },
+        closeModal: function () {
+            this.show = false;
+        },
+    }
+})
+
 new Vue({
     el: '#modal-image',
     data: {
         showModal: false,
     }
 })
- 
+
 new Vue({
-    el: '#modal',
+    el: '#item-modal',
     data: {
         showModal: false,
+    },
+    methods: {
+        openModal: function () {
+            this.showModal = true;
+        },
+        closeModal: function () {
+            this.showModal = false;
+        },
     }
 })
+
+new Vue({
+    el: '#thread-modal',
+    data: {
+        showModal: false,
+    },
+    methods: {
+        openModal: function () {
+            this.showModal = true;
+        },
+        closeModal: function () {
+            this.showModal = false;
+        },
+    }
+})
+
+function test() {
+    console.log(document.getElementById("test"));
+}
+
+
+function hoge(e) {
+    console.log(e.target);
+    console.log(e.target.querySelector("img"));
+}
+var el = document.getElementById("test");
+//el.addEventListener("click",hoge, false);
